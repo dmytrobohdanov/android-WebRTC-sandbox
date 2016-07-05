8e7827a8749ba11477de880eebdd809a9ef2828f
dmytrobohdanov <dmytro.bohdanov@gmail.com>
2016-07-05 19:34:59 +0300

    init commit

---
+++ app/src/main/java/com/dmytro/webrtctesting/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dmytro.webrtctesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
