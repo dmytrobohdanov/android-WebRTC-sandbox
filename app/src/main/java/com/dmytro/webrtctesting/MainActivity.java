44ea7534f138f2bb9589e763c82c3a861ef76366
dmytrobohdanov <dmytro.bohdanov@gmail.com>
2016-07-08 22:33:19 +0300

    self-streaming done

--- app/src/main/java/com/dmytro/webrtctesting/MainActivity.java
+++ app/src/main/java/com/dmytro/webrtctesting/MainActivity.java
@@ -16,68 +16,49 @@ import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
 
+/**
+ * Playing with WebRTC on Android
+ * using https://tech.appear.in/2015/05/25/Introduction-to-WebRTC-on-Android/
+ */
 public class MainActivity extends AppCompatActivity {
     private static final String VIDEO_TRACK_ID = "id001";
     private static final String AUDIO_TRACK_ID = VIDEO_TRACK_ID;
     private final boolean INITIALIZE_AUDIO = true;
-    private final boolean INITIALIZE_VIDEO = false;
-    private final boolean VIDEO_CODEC_HW_ACCELERATION = false;
+    private final boolean INITIALIZE_VIDEO = true;
+    private final boolean VIDEO_CODEC_HW_ACCELERATION = true;
     private GLSurfaceView videoView;
     private VideoTrack localVideoTrack;
 
-    private final String D_TAG = "MainActivity";
+    private final String D_TAG = "111MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(D_TAG, "start");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(D_TAG, "2");
-        //todo: check is everything ok and initializeAndroidGlobals returned TRUE
-        boolean peerConnection = PeerConnectionFactory.initializeAndroidGlobals(
-                this,
-                INITIALIZE_AUDIO,
-                INITIALIZE_VIDEO,
-                VIDEO_CODEC_HW_ACCELERATION,
-                this);
-        Log.d(D_TAG, "" + peerConnection);
+        videoView = (GLSurfaceView) findViewById(R.id.surfaceView);
+
+        Log.d(D_TAG, "version 0.0.0.4");
+
+        videoView.setEGLContextClientVersion(1);
+        boolean peerConnection = PeerConnectionFactory.initializeAndroidGlobals(this, INITIALIZE_AUDIO, INITIALIZE_VIDEO, VIDEO_CODEC_HW_ACCELERATION, null);
+        //todo: check peerConnection for success
 
         PeerConnectionFactory peerConnectionFactory = new PeerConnectionFactory();
 
-        Log.d(D_TAG, "factory");
-
         // Returns the number of camera devices
-        VideoCapturerAndroid.getDeviceCount();
+//        int numberOfCameras= VideoCapturerAndroid.getDeviceCount();
+//        String name = VideoCapturerAndroid.getNameOfFrontFacingDevice();
+//        String name = VideoCapturerAndroid.getNameOfBackFacingDevice();
 
-        Log.d(D_TAG, "get device");
-
-        // Returns the front face device name
-        Log.d(D_TAG, VideoCapturerAndroid.getNameOfFrontFacingDevice());
-//        VideoCapturerAndroid.getNameOfFrontFacingDevice();
-        // Returns the back facing device name
-        Log.d(D_TAG, VideoCapturerAndroid.getNameOfBackFacingDevice());
-//        VideoCapturerAndroid.getNameOfBackFacingDevice();
-
-        Log.d(D_TAG, "names");
+        String name = VideoCapturerAndroid.getDeviceName(0);
 
         // Creates a VideoCapturerAndroid instance for the device name
-        VideoCapturerAndroid capturer =
-                VideoCapturerAndroid.create(VideoCapturerAndroid.getNameOfBackFacingDevice());
-//                VideoCapturerAndroid.create(VideoCapturerAndroid.getNameOfFrontFacingDevice());
-
-        Log.d(D_TAG, "init capturer");
+        VideoCapturerAndroid capturer = VideoCapturerAndroid.create(name);
 
         MediaConstraints videoConstraints = new MediaConstraints();
 
-        Log.d(D_TAG, "pinggg!");
-
-
         // First we create a VideoSource
-        //todo problem is here
-        VideoSource videoSource =
-                peerConnectionFactory.createVideoSource(capturer, videoConstraints);
-
-        Log.d(D_TAG, "videoSource");
+        VideoSource videoSource = peerConnectionFactory.createVideoSource(capturer, videoConstraints);
 
         // Once we have that, we can create our VideoTrack
         // Note that VIDEO_TRACK_ID can be any string that uniquely
@@ -90,55 +71,26 @@ public class MainActivity extends AppCompatActivity {
         AudioSource audioSource =
                 peerConnectionFactory.createAudioSource(audioConstraints);
 
-        Log.d(D_TAG, "audio source");
-
         // Once we have that, we can create our AudioTrack
         // Note that AUDIO_TRACK_ID can be any string that uniquely
         // identifies that audio track in your application
         AudioTrack localAudioTrack =
                 peerConnectionFactory.createAudioTrack(AUDIO_TRACK_ID, audioSource);
 
-
-        Log.d(D_TAG, "audio 2");
-
-        // To create our VideoRenderer, we can use the
-        // included VideoRendererGui for simplicity
-        // First we need to set the GLSurfaceView that it should render to
-//        videoView = (GLSurfaceView) findViewById(R.id.surfaceView);
-
-        // Then we set that view, and pass a Runnable
-        // to run once the surface is ready
         VideoRendererGui.setView(videoView, new Runnable() {
             @Override
             public void run() {
-                Log.d(D_TAG, "run()");
-                videoView = (GLSurfaceView) findViewById(R.id.surfaceView);
-
-                // Now that VideoRendererGui is ready, we can get our VideoRenderer
-                VideoRenderer renderer = null;
-                try {
-                    Log.d(D_TAG, "try");
-                    renderer = VideoRendererGui.createGui(
-                            0,
-                            0,
-//                            videoView.getWidth(),
-//                            videoView.getHeight(),
-                            600, //todo remove
-                            480, //todo remove
-                            VideoRendererGui.ScalingType.SCALE_ASPECT_FILL,
-                            false);
-                    Log.d(D_TAG, "try - done");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                // And finally, with our VideoRenderer ready, we
-                // can add our renderer to the VideoTrack.
-                assert renderer != null;
-                localVideoTrack.addRenderer(renderer);
-                Log.d(D_TAG, "end run");
             }
         });
+
+        try {
+            //todo change sizes to constants
+            VideoRenderer renderer = VideoRendererGui.createGui(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FIT, false);
+            localVideoTrack.addRenderer(renderer);
+        } catch (Exception e) {
+            String s = e.getMessage();
+            Log.e(D_TAG, s);
+        }
 
 
     }
